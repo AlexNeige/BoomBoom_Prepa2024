0c1143a65986df9222f63502efc70084e5ac1e1e
AlexNeige <lagealex9@gmail.com>
2024-02-08 14:09:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boom Boom</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <div id="logo">
+        <img src="./assets/img/heart-7686125_1280.png" alt="">
+    </div>
+    <div id="connect">
+        <a href="./register.php">
+            <button>Connecte-toi!</button>
+    </a>
+    </div>
+    
+</body>
+</html>
---
+++ users.js
@@ -0,0 +1,40 @@
+//creation d'un tableau pour nos utilisateurs
+const userlist = [
+    {
+       
+        email:"john@doe.fr",
+        passeword:"azerty",
+       
+        avatar:"http://fakeavatar.com",
+        
+        nom:"Doe",
+        prenom:"john",
+        age:26,
+        sexe:"homme",
+        
+        
+        telephone:"0606060602",
+        adresse:"nguetueuhguhrugh",
+         
+        profil:"Hello, my name is: John Doe!",
+        tags: ["HIPHOP","POP Finlandaise"]
+    },
+    {
+        email:"jane@balec.fr",
+        passeword:"azerty",
+       
+        avatar:"http://fakeavatar.com",
+        
+        nom:"Balec",
+        prenom:"Jane",
+        age:26,
+        sexe:"Femme",
+        
+        
+        telephone:"0606060602",
+        adresse:"nguetueuhguhrugh",
+         
+        profil:"Hello, my name is: Jane Balec!",
+        tags: ["HIPHOP","POP Finlandaise"]
+    }
+]
